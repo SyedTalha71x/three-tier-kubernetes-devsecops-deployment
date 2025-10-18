ceb9b37e0f3210a07be68544e158b6bbfd9099b7
SyedTalha71x <syedtalha71x@gmail.com>
2025-10-18 18:32:00 +0500

    Commit: Update pipeline

--- application/backend/index.js
+++ application/backend/index.js
@@ -11,8 +11,6 @@ app.use(express.json());
 app.use(cors());
 
 // Health check endpoints
-
-// Basic health check to see if the server is running
 app.get('/healthz', (req, res) => {
     res.status(200).send('Healthy');
 });
